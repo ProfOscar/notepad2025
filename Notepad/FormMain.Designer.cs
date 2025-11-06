7ee98f5fb15b08cfd86764b4df113e4b00a0ff9b
Oscar Cambieri <oscar.cambieri@vallauri.edu>
2025-11-06 12:39:12 +0100

    Aggiunto menu strip, status strip, richtext

---
+++ Notepad/FormMain.Designer.cs
@@ -0,0 +1,84 @@
+﻿namespace Notepad
+{
+    partial class FormMain
+    {
+        /// <summary>
+        /// Variabile di progettazione necessaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Pulire le risorse in uso.
+        /// </summary>
+        /// <param name="disposing">ha valore true se le risorse gestite devono essere eliminate, false in caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Codice generato da Progettazione Windows Form
+
+        /// <summary>
+        /// Metodo necessario per il supporto della finestra di progettazione. Non modificare
+        /// il contenuto del metodo con l'editor di codice.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.menuStripTop = new System.Windows.Forms.MenuStrip();
+            this.statusStripBottom = new System.Windows.Forms.StatusStrip();
+            this.rtbMain = new System.Windows.Forms.RichTextBox();
+            this.SuspendLayout();
+            // 
+            // menuStripTop
+            // 
+            this.menuStripTop.Location = new System.Drawing.Point(0, 0);
+            this.menuStripTop.Name = "menuStripTop";
+            this.menuStripTop.Size = new System.Drawing.Size(800, 24);
+            this.menuStripTop.TabIndex = 0;
+            this.menuStripTop.Text = "Menu";
+            // 
+            // statusStripBottom
+            // 
+            this.statusStripBottom.Location = new System.Drawing.Point(0, 428);
+            this.statusStripBottom.Name = "statusStripBottom";
+            this.statusStripBottom.Size = new System.Drawing.Size(800, 22);
+            this.statusStripBottom.TabIndex = 1;
+            this.statusStripBottom.Text = "Barra di Stato";
+            // 
+            // rtbMain
+            // 
+            this.rtbMain.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.rtbMain.Location = new System.Drawing.Point(0, 24);
+            this.rtbMain.Name = "rtbMain";
+            this.rtbMain.Size = new System.Drawing.Size(800, 404);
+            this.rtbMain.TabIndex = 2;
+            this.rtbMain.Text = "";
+            // 
+            // FormMain
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.rtbMain);
+            this.Controls.Add(this.statusStripBottom);
+            this.Controls.Add(this.menuStripTop);
+            this.MainMenuStrip = this.menuStripTop;
+            this.Name = "FormMain";
+            this.Text = "Blocco note di Windows";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.MenuStrip menuStripTop;
+        private System.Windows.Forms.StatusStrip statusStripBottom;
+        private System.Windows.Forms.RichTextBox rtbMain;
+    }
+}
+
